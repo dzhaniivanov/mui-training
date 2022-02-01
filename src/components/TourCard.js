dc06cf41c7001b2d9834d080efc8994930fd16dd
dzhani ivanov <djani.ivanov94@gmail.com>
2022-02-01 10:37:50 +0200

    work on card styling

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -3,6 +3,7 @@ import Grid from "@mui/material/Grid";
 import Typography from '@mui/material/Typography';
 import Box from "@mui/material/Box";
 import { AccessTime } from "@mui/icons-material";
+import Rating from '@mui/material/Rating';
 
 
 const TourCard = () => {
@@ -23,6 +24,32 @@ const TourCard = () => {
                         <AccessTime sx={{ width: 12.5 }} />
                         <Typography variant="body2" component="p" marginLeft={0.5}>
                             5 hours
+                        </Typography>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                        marginTop={3}
+                    >
+                        <Rating
+                            name="read-only"
+                            value={4.5}
+                            precision={0.5}
+                            readOnly
+                            size="small"
+                        />
+                        <Typography variant="body2" component="p" marginLeft={0.5}>
+                            4.5
+                        </Typography>
+                        <Typography variant="body3" component="p" marginLeft={1.5}>
+                            (655 reviews)
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" component="h3" marginTop={0}>
+                            $147
                         </Typography>
                     </Box>
                 </Box>
