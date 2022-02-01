038f559b69763af985a6eb319e29bcc640fb57f2
dzhani ivanov <djani.ivanov94@gmail.com>
2022-02-01 11:40:11 +0200

    work on images components

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,7 +1,23 @@
+import { Box, Container, Typography } from "@mui/material";
+import ImageCollage from "../components/ImageCollage";
 
 
 const Tour = () => {
-  return <div>edfvgb</div>;
+    return (
+        <Container sx={{ width: 900 }}>
+            <Typography variant="h3" component="h1" marginTop={3}>
+                Explore the World in Vegas
+            </Typography>
+            <Box marginTop={3} sx={{ display: "flex" }}>
+                <img
+                    src="https://images.unsplash.com/photo-1605833556294-ea5c7a74f57d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFzJTIwdmVnYXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                    alt=""
+                    height={325}
+                />
+                <ImageCollage/>
+            </Box>
+        </Container>
+    );
 };
 
 export default Tour;
